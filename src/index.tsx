5e846f98d6edeec9de58c8496b3f3cf7187ab336
Hriteek Roushan <hriteekroshan68@gmail.com>
2025-09-16 13:18:43 +0530

    Enable MSW in production for Vercel deployment

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,13 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { worker } from './mocks/browser';
+
+// Start MSW in all environments (including production)
+worker.start();
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
